a174e86ed4f7b64d8a0adf6bbec1670200538ff9
Grégory Dozot <gregory.dozot@heig-vd.ch>
2022-12-02 14:52:50 +0100

    Setup test env

---
+++ test/components/drawer.spec.ts
@@ -0,0 +1,23 @@
+import { expect, describe, it } from 'vitest';
+import Map from 'ol/Map';
+import Drawer from '../../src/components/drawer';
+
+describe('Drawer class test suite', () => {
+  it('Ensure that the interaction is enabled', () => {
+    const map = new Map({target: 'test'});
+    const drawer = new Drawer(map);
+    drawer.typeSelect = new HTMLInputElement();
+    drawer.typeSelect.value = 'Point';
+    drawer.addInteraction();
+    expect(drawer.draw).toBeDefined()
+    expect(drawer.snap).toBeDefined()
+  })
+
+  it('Interaction is not available when parameter is wrong', () => {
+    const map = new Map({target: 'test'});
+    const drawer = new Drawer(map);
+    drawer.addInteraction();
+    expect(drawer.draw).toBeUndefined()
+    expect(drawer.snap).toBeUndefined()
+  })
+})
