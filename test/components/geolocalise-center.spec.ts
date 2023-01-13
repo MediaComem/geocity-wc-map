51376d48dfdf9d2b3f7df37a6e038577e5823987
Grégory Dozot <gregory.dozot@heig-vd.ch>
2023-01-13 10:47:04 +0100

    Fix test GeolocationCenter

--- test/components/geolocalise-center.spec.ts
+++ test/components/geolocalise-center.spec.ts
@@ -1,14 +1,14 @@
 import { expect, describe, it } from 'vitest';
 import Map from 'ol/Map';
-import GeolocaliseCenter from '../../src/components/geolicalise-center';
+import GeolocationCenter from '../../src/components/geolocation-center';
 import { Geolocation, View } from 'ol';
 
-describe('GeolocaliseCenter class test suite', () => {
-  it('GeolocatiseCenter creation', () => {
+describe('GeolocationCenter class test suite', () => {
+  it('GeolocationCenter creation', () => {
     const map = new Map({target: 'test'});
     const view = new View();
     const geolocation = new Geolocation();
-    const geolocationCenter = new GeolocaliseCenter(map, view, geolocation)
+    const geolocationCenter = new GeolocationCenter(map, view, geolocation)
     expect(geolocationCenter).toBeDefined();
   })
 })
