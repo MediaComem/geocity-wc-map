c99f0035ab4c384d0a0c00b1d8e3b660c77425c1
Grégory Dozot <gregory.dozot@heig-vd.ch>
2023-01-13 11:05:47 +0100

    Fix tests

--- test/components/drawer.spec.ts
+++ test/components/drawer.spec.ts
@@ -5,7 +5,7 @@ import Drawer from '../../src/components/drawer';
 describe('Drawer class test suite', () => {
   it('Ensure that the interaction is enabled', () => {
     const map = new Map({target: 'test'});
-    const drawer = new Drawer(map, document.documentElement);
+    const drawer = new Drawer(map, document.documentElement, true);
     drawer.typeSelect = new HTMLInputElement();
     drawer.typeSelect.value = 'Point';
     drawer.addInteraction();
@@ -15,7 +15,7 @@ describe('Drawer class test suite', () => {
 
   it('Interaction is not available when parameter is wrong', () => {
     const map = new Map({target: 'test'});
-    const drawer = new Drawer(map, document.documentElement);
+    const drawer = new Drawer(map, document.documentElement, true);
     drawer.addInteraction();
     expect(drawer.draw).toBeUndefined()
     expect(drawer.snap).toBeUndefined()
