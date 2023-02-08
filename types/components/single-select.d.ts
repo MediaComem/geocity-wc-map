85bfffc2f268119059d82bede8a64304378f798f
Grégory Dozot <gregory.dozot@heig-vd.ch>
2023-02-08 15:47:48 +0100

    Update styles

--- types/components/single-select.d.ts
+++ types/components/single-select.d.ts
@@ -1,3 +1,11 @@
+import Map from 'ol/Map';
+import { Vector } from 'ol/source';
+import { Vector as VectorLayer } from 'ol/layer';
+import { Geometry } from 'ol/geom';
+import IOption from '../utils/options';
 export default class SingleSelect {
     constructor();
+    parseToGeometry(json: string): Vector<Geometry>;
+    displayDataOnMap(map: Map, json: string, vectorLayer: VectorLayer<Vector<Geometry>>, options: IOption): void;
+    toogleDataSelection(map: Map, vectorLayer: VectorLayer<Vector<Geometry>>): void;
 }
