9d1a33dc09ad7ae455b2d2b38e91eff9ec09a882
Grégory Dozot <gregory.dozot@heig-vd.ch>
2023-02-08 15:47:18 +0100

    Refactor single select code

--- src/components/single-select.ts
+++ src/components/single-select.ts
@@ -1,128 +1,31 @@
 import { Feature } from 'ol';
+import Map from 'ol/Map';
 import { Cluster, Vector } from 'ol/source';
 import { Vector as VectorLayer } from 'ol/layer';
-import { Circle, Point } from 'ol/geom';
-
+import { Geometry, Point } from 'ol/geom';
 import { parse } from 'ol/xml';
-import { Fill, Icon, Stroke, Style, Text } from 'ol/style';
-import CircleStyle from 'ol/style/Circle';
 
 import { GeocityEvent } from '../utils/geocity-event';
-import SVGCreator from '../utils/svg-creator';
 import SelectInformationBoxController from './select-information-box';
+
 import { useStore } from '../composable/store';
+import SingleSelectStyle from './styles/single-select-style';
+import { Control } from 'ol/control';
+import IOption from '../utils/options';
 
 export default class SingleSelect {
   constructor() {
     const map = useStore().getMap();
     const options = useStore().getOptions();
     const vectorLayer = new VectorLayer();
-    const vectorSource = new Vector;
 
     fetch(options.wfs.url)
       .then((response) => {
         return response.text();
       })
       .then((json) => {
-        const xmlDoc = parse(json);
-        const features = xmlDoc.getElementsByTagName(
-          'wfs:FeatureCollection'
-        )[0];
-        const layers = features.getElementsByTagName('wfs:member');
-        for (let i = 0; i < layers.length; i++) {
-          const geom = layers[i].getElementsByTagName('ms:geom')[0];
-          const point = geom.getElementsByTagName('gml:Point')[0];
-          const pos = point.getElementsByTagName('gml:pos')[0];
-          const coordinates = pos.innerHTML.split(" ");
-          const geomPoint = new Point([Number(coordinates[0]), Number(coordinates[1])]);
-          const marker = new Feature({
-            geometry: geomPoint,
-            name: layers[i].getElementsByTagName('ms:objectid')[0].innerHTML,
-            myCustomeValue: layers[i],
-            isClick: false,
-          });
-          vectorSource?.addFeature(marker);
-        }
-        
-        const clusterSource = new Cluster({
-          distance: options.cluster.distance,
-          minDistance: options.cluster.minDistance,
-          source: vectorSource,
-        });
-        
-        vectorLayer.setSource(clusterSource)
-        vectorLayer.setStyle(function (feature) {
-          const size = feature.get('features').length;
-          let style;
-          if(size === 1 && feature.get('features')[0].get('isClick'))
-            {
-              style = new Style({
-                zIndex: 1,
-                image: new Icon({
-                  src: 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent( SVGCreator.mapPinSelect ),
-                  // the svg height is 54px. It's the reason why the anchor is set like that
-                  anchor: [0.5, 54],
-                  anchorXUnits: 'fraction',
-                  anchorYUnits: 'pixels',
-                })
-              })
-            }
-          else if (size === 1 && !feature.get('features')[0].get('isClick'))
-            {
-              style = new Style({
-                zIndex: 1,
-                image: new Icon({
-                  src: 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent( SVGCreator.mapPin ),
-                  // the svg height is 54px. It's the reason why the anchor is set like that
-                  anchor: [0.5, 54],
-                  anchorXUnits: 'fraction',
-                  anchorYUnits: 'pixels',
-                })
-              })
-            }
-          else if (feature.get('features').find((f:Feature) => f.get('isClick'))) {
-            style = new Style({
-              image: new CircleStyle({
-                radius: 15,
-                stroke: new Stroke({
-                  color: '#fff',
-                }),
-                fill: new Fill({
-                  color: '#EF4444',
-                }),
-              }),
-              text: new Text({
-                text: size.toString(),
-                font: "14px sans-serif",
-                fill: new Fill({
-                  color: '#fff',
-                }),
-              }),
-            });
-          }
-          else {
-            style = new Style({
-              image: new CircleStyle({
-                radius: 15,
-                stroke: new Stroke({
-                  color: '#fff',
-                }),
-                fill: new Fill({
-                  color: '#334155',
-                }),
-              }),
-              text: new Text({
-                text: size.toString(),
-                font: "14px sans-serif",
-                fill: new Fill({
-                  color: '#fff',
-                }),
-              }),
-            });
-          }
-          return style;
-        },)
-        map.addLayer(vectorLayer);
+        this.displayDataOnMap(map, json, vectorLayer, options)
+        // Setup selection
         if (options.mode.type === 'select') {
           map.on('click', function (evt) {
             map.forEachFeatureAtPixel(evt.pixel, function (feature) {
@@ -135,52 +38,92 @@ export default class SingleSelect {
             });
           });
 
-          window.addEventListener('authorize-clicked', () => {
-            const feature = useStore().getSelectedFeature();
-            if (feature) {
-              const currentState = feature.get('isClick')
-              if (currentState) {
-                feature.set('isClick', false)
-                map.getControls().forEach((control) => {
-                  if (control instanceof SelectInformationBoxController) {
-                      map.removeControl(control);
-                  }
-                });
-                useStore().setCustomDisplay(false);
-                useStore().setTargetBoxSize('no-box');
-                useStore().setSelectedFeature(undefined);
-              } else {
-                vectorLayer.getSource()?.getFeatures().forEach((f) => f.get('features').forEach((f2:Feature) => {
-                  f2.set('isClick', false);
-                  map.getControls().forEach((control) => {
-                    if (control instanceof SelectInformationBoxController) {
-                      map.removeControl(control);
-                    }
-                  });
-                }))
-                feature.set('isClick', true);
-                useStore().setCustomDisplay(true);
-                map.addControl(new SelectInformationBoxController(feature.get('geometry').getCoordinates()));
-                useStore().setTargetBoxSize('medium');
-              }
-            }
-            useStore().getMap().get('target').className = `${useStore().getTargetBoxSize()} ${useStore().getTheme()}`
-          })
+          this.toogleDataSelection(map, vectorLayer);
         }
 
         window.addEventListener('recenter-selected-element', () => {
           useStore().getMap().getView().setCenter(useStore().getSelectedFeature()?.get('geometry').getCoordinates())
         })        
       });
+  }
 
-    window.addEventListener('current-center-position', ((event: CustomEvent) => {
-      const nearestPoint = vectorSource.getClosestFeatureToCoordinate(event.detail);
-      const circle = new Circle(event.detail, options.mode.radius);
-      if (nearestPoint.getGeometry()?.getType() === 'Point') {
-        const nearestPointCoordinate = (nearestPoint?.getGeometry() as Point).getCoordinates();
-        // This event will use by the notification manager to inform about an nearest POI.
-        if (circle.intersectsCoordinate(nearestPointCoordinate)) GeocityEvent.sendEvent('nearest-poi-position', nearestPointCoordinate);
+  parseToGeometry(json:string) {
+    const vectorSource = new Vector; 
+    const xmlDoc = parse(json);
+    const features = xmlDoc.getElementsByTagName(
+      'wfs:FeatureCollection'
+    )[0];
+    const layers = features.getElementsByTagName('wfs:member');
+    for (let i = 0; i < layers.length; i++) {
+      const geom = layers[i].getElementsByTagName('ms:geom')[0];
+      const point = geom.getElementsByTagName('gml:Point')[0];
+      const pos = point.getElementsByTagName('gml:pos')[0];
+      const coordinates = pos.innerHTML.split(" ");
+      const geomPoint = new Point([Number(coordinates[0]), Number(coordinates[1])]);
+      const marker = new Feature({
+        geometry: geomPoint,
+        name: layers[i].getElementsByTagName('ms:objectid')[0].innerHTML,
+        myCustomeValue: layers[i],
+        isClick: false,
+      });
+      vectorSource.addFeature(marker);
+    }
+    return vectorSource;
+  }
+
+  displayDataOnMap(map: Map, json: string ,vectorLayer: VectorLayer<Vector<Geometry>>, options: IOption) {
+    const clusterSource = new Cluster({
+      distance: options.cluster.distance,
+      minDistance: options.cluster.minDistance,
+      source: this.parseToGeometry(json),
+    });
+    
+    vectorLayer.setSource(clusterSource)
+    vectorLayer.setStyle(function (feature) {          
+      return SingleSelectStyle.clusterWithIcon(feature);
+    },)
+
+    map.addLayer(vectorLayer);
+  }
+
+  /*
+    Check the selection state to check or uncheck the selected event.
+    In addition, unselect all selected events if other icon is selected to keep only one selected element
+  */
+  toogleDataSelection(map: Map, vectorLayer: VectorLayer<Vector<Geometry>>) {
+    window.addEventListener('authorize-clicked', () => {
+      const feature = useStore().getSelectedFeature();
+      if (feature) {
+        const currentState = feature.get('isClick')
+        if (currentState) {
+          feature.set('isClick', false)
+          map.getControls().forEach((control: Control) => {
+            if (control instanceof SelectInformationBoxController) {
+                map.removeControl(control);
+            }
+          });
+          // Set parameter for icon position display
+          useStore().setCustomDisplay(false);
+          useStore().setTargetBoxSize('no-box');
+          useStore().setSelectedFeature(undefined);
+        } else {
+          // Search all selected icon to deselect them and ensure that only one element is selected.
+          vectorLayer.getSource()?.getFeatures().forEach((feature) => feature.get('features').forEach((geometryFeature:Feature) => {
+            geometryFeature.set('isClick', false);
+            map.getControls().forEach((control: Control) => {
+              if (control instanceof SelectInformationBoxController) {
+                map.removeControl(control);
+              }
+            });
+          }));
+          feature.set('isClick', true);
+          map.addControl(new SelectInformationBoxController(feature.get('geometry').getCoordinates()));
+          useStore().setCustomDisplay(true);
+          useStore().setTargetBoxSize('medium');
+        }
       }
-    }) as EventListener)
+      // Set right class to the map
+      useStore().getMap().get('target').className = `${useStore().getTargetBoxSize()} ${useStore().getTheme()}`
+    })
   }
 }
