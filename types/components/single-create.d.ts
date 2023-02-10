07eeed2f2ffaeebd64e9401b728e10e4f8dd2e49
Grégory Dozot <gregory.dozot@heig-vd.ch>
2023-02-10 13:24:45 +0100

    Fix problem validation when rule revalidate

--- types/components/single-create.d.ts
+++ types/components/single-create.d.ts
@@ -1,5 +1,11 @@
 import Feature from 'ol/Feature';
+import { Vector } from 'ol/source';
+import { Map } from 'ol';
 export default class SingleCreate {
     currentFeature: Feature | undefined;
     constructor(mapElement: HTMLDivElement);
+    setupMapForCreation(map: Map, vectorSource: Vector): void;
+    setupCreateElement(map: Map, vectorSource: Vector): void;
+    setupDeleteElement(map: Map, vectorSource: Vector): void;
+    longClickEvent(mapElement: HTMLDivElement, map: Map): void;
 }
