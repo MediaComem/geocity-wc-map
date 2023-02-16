fbfda9436e7b5c1b1eb087bde2ade89e596c9d68
Grégory Dozot <gregory.dozot@heig-vd.ch>
2023-02-16 15:12:38 +0100

    Implement WFS format way to load WFS + documentation

--- src/components/single-select.ts
+++ src/components/single-select.ts
@@ -2,8 +2,8 @@ import { Feature } from 'ol';
 import Map from 'ol/Map';
 import { Cluster, Vector } from 'ol/source';
 import { Vector as VectorLayer } from 'ol/layer';
-import { Geometry, Point } from 'ol/geom';
-import { parse } from 'ol/xml';
+import { Geometry } from 'ol/geom';
+import VectorSource from "ol/source/Vector.js";
 
 import { GeocityEvent } from '../utils/geocity-event';
 import SelectCreateInformationBoxController from './select-create-information-box';
@@ -12,6 +12,7 @@ import { useStore } from '../composable/store';
 import SingleSelectStyle from './styles/single-select-style';
 import IOption from '../utils/options';
 import CustomStyleSelection from '../utils/custom-style-selection';
+import WFSLoader from '../utils/wfs-loader';
 
 export default class SingleSelect {
 
@@ -21,64 +22,31 @@ export default class SingleSelect {
     const map = useStore().getMap();
     const options = useStore().getOptions();
     const vectorLayer = new VectorLayer();
+    const vectorSource = WFSLoader.getSource(useStore().getOptions().wfs.url, '')
+  
+    this.displayDataOnMap(map, vectorLayer, options, vectorSource);
 
-    fetch(options.wfs.url)
-      .then((response) => {
-        return response.text();
-      })
-      .then((json) => {
-        this.displayDataOnMap(map, json, vectorLayer, options)
-        // Setup selection
-        if (options.mode.type === 'select') {
-          map.on('click', function (evt) {
-            map.forEachFeatureAtPixel(evt.pixel, function (feature) {
-              if (feature && feature.getGeometry()?.getType() === 'Point') {
-                if (feature.getProperties().features.length === 1) {
-                  useStore().setSelectedFeature(feature.getProperties().features[0]);
-                  GeocityEvent.sendEvent('icon-clicked', undefined);                
-                }
-              }
-            });
-          });
-
-          this.toogleDataSelection(vectorLayer);
+    map.on('click', function (evt) {
+      map.forEachFeatureAtPixel(evt.pixel, function (feature) {
+        if (feature && feature.getGeometry()?.getType() === 'Point') {
+          if (feature.getProperties().features.length === 1) {
+            useStore().setSelectedFeature(feature.getProperties().features[0]);
+            GeocityEvent.sendEvent('icon-clicked', undefined);                
+          }
         }
-
-        window.addEventListener('recenter-selected-element', () => {
-          useStore().getMap().getView().setCenter(useStore().getSelectedFeature()?.get('geometry').getCoordinates())
-        })        
       });
+    });
+
+    window.addEventListener('recenter-selected-element', () => {
+      useStore().getMap().getView().setCenter(useStore().getSelectedFeature()?.get('geom').getCoordinates())
+    })    
   }
 
-  parseToGeometry(json:string) {
-    const vectorSource = new Vector; 
-    const xmlDoc = parse(json);
-    const features = xmlDoc.getElementsByTagName(
-      'wfs:FeatureCollection'
-    )[0];
-    const layers = features.getElementsByTagName('wfs:member');
-    for (let i = 0; i < layers.length; i++) {
-      const geom = layers[i].getElementsByTagName('ms:geom')[0];
-      const point = geom.getElementsByTagName('gml:Point')[0];
-      const pos = point.getElementsByTagName('gml:pos')[0];
-      const coordinates = pos.innerHTML.split(" ");
-      const geomPoint = new Point([Number(coordinates[0]), Number(coordinates[1])]);
-      const marker = new Feature({
-        geometry: geomPoint,
-        name: layers[i].getElementsByTagName('ms:objectid')[0].innerHTML,
-        myCustomeValue: layers[i],
-        isClick: false,
-      });
-      vectorSource.addFeature(marker);
-    }
-    return vectorSource;
-  }
-
-  displayDataOnMap(map: Map, json: string ,vectorLayer: VectorLayer<Vector<Geometry>>, options: IOption) {
+  displayDataOnMap(map: Map, vectorLayer: VectorLayer<Vector<Geometry>>, options: IOption, vectorSource: VectorSource) {
     const clusterSource = new Cluster({
       distance: options.cluster.distance,
       minDistance: options.cluster.minDistance,
-      source: this.parseToGeometry(json),
+      source: vectorSource,
     });
     
     vectorLayer.setSource(clusterSource)
@@ -90,6 +58,7 @@ export default class SingleSelect {
 
     this.control.disable();
     map.addControl(this.control);
+    this.toogleDataSelection(vectorLayer);
   }
 
   /*
@@ -102,18 +71,20 @@ export default class SingleSelect {
       if (feature) {
         const currentState = feature.get('isClick')
         if (currentState) {
-          feature.set('isClick', false)
+          feature.set('isClick', undefined)
           this.control.hide();
           // Set parameter for icon position display
           CustomStyleSelection.setCustomStyleWithouInfoBox();
         } else {
           // Search all selected icon to deselect them and ensure that only one element is selected.
-          vectorLayer.getSource()?.getFeatures().forEach((feature) => feature.get('features').forEach((geometryFeature:Feature) => {
-            geometryFeature.set('isClick', false);
-          }));
+          vectorLayer.getSource()?.getFeatures().forEach((feature) => {
+            feature.get('features').forEach((geometryFeature:Feature) => {
+              geometryFeature.set('isClick', undefined);
+            });
+          });
           feature.set('isClick', true);
           this.control.show()
-          GeocityEvent.sendEvent('open-select-create-box', feature.get('geometry').getCoordinates())
+          GeocityEvent.sendEvent('open-select-create-box', feature.get('geom').getCoordinates())
           useStore().setCustomDisplay(true);
           useStore().setTargetBoxSize('select');
         }
@@ -121,5 +92,6 @@ export default class SingleSelect {
       // Set right class to the map
       useStore().getMap().get('target').className = `${useStore().getTargetBoxSize()} ${useStore().getTheme()}`
     })
+    
   }
 }
---
+++ types/types/inclusion-area-config.d.ts
@@ -0,0 +1,4 @@
+export default interface InclusionAreaConfig {
+    url: string;
+    filter: string;
+}
