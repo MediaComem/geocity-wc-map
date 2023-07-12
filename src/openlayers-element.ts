751d5a8b818fc45a3e0aed3691d3301f786cbc0a
Grégory Dozot <gregory.dozot@heig-vd.ch>
2023-07-12 07:57:42 +0200

    Read only update mode fix

--- src/openlayers-element.ts
+++ src/openlayers-element.ts
@@ -7,8 +7,6 @@ import { Geolocation } from 'ol';
 import { ScaleLine } from 'ol/control';
 import SingleSelect from './components/mode/select';
 
-
-
 import WMTSLoader from './components/mapView/wmts-loader';
 
 import styles from '../node_modules/ol/ol.css?inline';
@@ -31,7 +29,7 @@ import ControlIconManager from './utils/control-icon-manager';
 import TargetController from './components/mode/target';
 import TargetInformationBoxElement from './components/notification/target-information-box';
 import proj4 from 'proj4';
-import {register} from 'ol/proj/proj4.js';
+import { register } from 'ol/proj/proj4.js';
 import SingleCreate from './components/mode/create';
 import SearchLocationControl from './components/control/search-location';
 import Border from './components/constraint/border';
@@ -54,41 +52,39 @@ export class OpenLayersElement extends LitElement {
   @query('#map')
   public mapElement!: HTMLDivElement;
 
-  @state() view:View | undefined;
-  @state() modeControllers: Array<SingleCreate | SingleSelect | TargetRenderer> = [];
+  @state() view: View | undefined;
+  @state() modeControllers: Array<
+    SingleCreate | SingleSelect | TargetRenderer
+  > = [];
   @state() renderUtils: Render;
   @state() inclusionArea: InclusionArea | undefined = undefined;
 
-  @property({type: Object, attribute: 'options'}) options = {}
+  @property({ type: Object, attribute: 'options' }) options = {};
 
-  @property({type: Object, attribute: 'states'}) states = {}
+  @property({ type: Object, attribute: 'states' }) states = {};
 
   store: Store;
 
   constructor() {
     super();
     this.store = new Store();
-    this.renderUtils = new Render(this.store)
-;  }
+    this.renderUtils = new Render(this.store);
+  }
 
   connectedCallback() {
     super.connectedCallback();
   }
 
-  setupTheme(options:any) {
+  setupTheme(options: any) {
     if (options.darkMode) {
       Store.setTheme('dark');
-    }
-    else if (options.lightMode) {
+    } else if (options.lightMode) {
       Store.setTheme('light');
-    }
-    else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+    } else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
       Store.setTheme('light');
-    }
-    else if (window.matchMedia('(prefers-color-scheme: dark)').matches) { 
+    } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
       Store.setTheme('dark');
-    }
-    else {
+    } else {
       Store.setTheme('light');
     }
   }
@@ -114,31 +110,52 @@ export class OpenLayersElement extends LitElement {
       - geolocationInformation.reverseLocation and geolocationInformation.currentLocation have the value false. This means that there is no line under the title (small size).
   */
   setupTargetBoxSize(geolocationInformation: GeolocationInformation) {
-    if (geolocationInformation.currentLocation && geolocationInformation.reverseLocation) this.store.setTargetBoxSize('large');
-    else if (geolocationInformation.currentLocation || geolocationInformation.reverseLocation) this.store.setTargetBoxSize('medium');
+    if (
+      geolocationInformation.currentLocation &&
+      geolocationInformation.reverseLocation
+    )
+      this.store.setTargetBoxSize('large');
+    else if (
+      geolocationInformation.currentLocation ||
+      geolocationInformation.reverseLocation
+    )
+      this.store.setTargetBoxSize('medium');
     else this.store.setTargetBoxSize('small');
   }
 
   firstUpdated() {
     const options = Options.webComponentOptions(this.options as IOption);
-    this.store.setOptions(options)
+    this.store.setOptions(options);
     const states = States.getStates(this.states as IStates);
-    if(!options) {
+    if (!options) {
       return;
     }
     const readonly = states.readonly;
     this.setupTheme(options);
     this.setupCustomDisplay(options);
-    proj4.defs('EPSG:2056', '+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs');
+    proj4.defs(
+      'EPSG:2056',
+      '+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs'
+    );
     register(proj4);
 
     this.view = new View({
       projection: 'EPSG:2056',
-      center: states.readonly && states.currentSelections && states.currentSelections.length == 1 ? states.currentSelections[0] : options.defaultCenter,
-      zoom: states.readonly && states.currentSelections && states.currentSelections.length == 1 ? options.maxZoom : options.zoom,
+      center:
+        states.readonly &&
+        states.currentSelections &&
+        states.currentSelections.length == 1
+          ? states.currentSelections[0]
+          : options.defaultCenter,
+      zoom:
+        states.readonly &&
+        states.currentSelections &&
+        states.currentSelections.length == 1
+          ? options.maxZoom
+          : options.zoom,
       minZoom: options.minZoom,
       maxZoom: options.maxZoom,
-      enableRotation: options.interaction.enableRotation
+      enableRotation: options.interaction.enableRotation,
     });
 
     const map = new Map({
@@ -150,40 +167,66 @@ export class OpenLayersElement extends LitElement {
     this.store.setMap(map);
     ControlIconManager.setupIcon(states, this.store);
     if (options.interaction.enableGeolocation && !readonly) {
-      Store.setGeolocation(new Geolocation({
-        trackingOptions: {
-          enableHighAccuracy: true,
-        },
-        projection: this.view.getProjection(),
-      }));
+      Store.setGeolocation(
+        new Geolocation({
+          trackingOptions: {
+            enableHighAccuracy: true,
+          },
+          projection: this.view.getProjection(),
+        })
+      );
       new GeolocationManager(map);
     }
-    if (options.search.displaySearch && options.mode.type !== 'target' && !readonly) { 
+    if (
+      options.search.displaySearch &&
+      options.mode.type !== 'target' &&
+      !readonly
+    ) {
       map.addControl(new SearchLocationControl(this.store, map));
     }
     if (options.mode.type === 'target') {
-      this.modeControllers.push(new TargetRenderer(this.renderUtils))
+      this.modeControllers.push(new TargetRenderer(this.renderUtils));
       if (!readonly) {
         map.addControl(new TargetController(this.store));
         if (options.geolocationInformation.displayBox)
-          map.addControl(
-            new TargetInformationBoxElement(this.store)
-          );
+          map.addControl(new TargetInformationBoxElement(this.store));
       }
     }
     if (options.wmts.length > 0) new WMTSLoader(this.store);
-    if (options.interaction.displayScaleLine) map.addControl(new ScaleLine({units: 'metric'}));
+    if (options.interaction.displayScaleLine)
+      map.addControl(new ScaleLine({ units: 'metric' }));
     if (options.border.url !== '') new Border(this.store, states);
-    if (options.inclusionArea.url !== '') this.inclusionArea = new InclusionArea(this.store);
+    if (options.inclusionArea.url !== '')
+      this.inclusionArea = new InclusionArea(this.store);
     if (options.mode.type === 'select' && options.wfs.url != '') {
-      this.modeControllers.push(new SingleSelect(this.renderUtils, states, this.store));
+      this.modeControllers.push(
+        new SingleSelect(this.renderUtils, states, this.store)
+      );
     }
     if (options.mode.type === 'create') {
-      this.modeControllers.push(new SingleCreate(this.mapElement, this.inclusionArea, this.renderUtils, states, this.store));
+      this.modeControllers.push(
+        new SingleCreate(
+          this.mapElement,
+          this.inclusionArea,
+          this.renderUtils,
+          states,
+          this.store
+        )
+      );
     }
     if (options.mode.type === 'mix' && options.wfs.url != '') {
-      this.modeControllers.push( new SingleSelect(this.renderUtils, states, this.store));
-      this.modeControllers.push(new SingleCreate(this.mapElement, this.inclusionArea, this.renderUtils, states, this.store));
+      this.modeControllers.push(
+        new SingleSelect(this.renderUtils, states, this.store)
+      );
+      this.modeControllers.push(
+        new SingleCreate(
+          this.mapElement,
+          this.inclusionArea,
+          this.renderUtils,
+          states,
+          this.store
+        )
+      );
     }
     if (!readonly) new NotificationManager(this.store);
     EventManager.setCursorEvent(map);
@@ -194,34 +237,86 @@ export class OpenLayersElement extends LitElement {
       if (this.states) {
         const states = States.getStates(this.states as IStates);
         if (states.currentSelections.length > 0) {
-          switch(this.store.getOptions()?.mode.type) {
-            case 'target': this.modeControllers[0]?.renderCurrentSelection(states); break;
-            case 'select': this.modeControllers[0]?.renderCurrentSelection(states); break;
-            case 'create': this.modeControllers[0]?.renderCurrentSelection(states); break;
-            case 'mix': this.renderUtils.displayMixMode(this.modeControllers[0]?.vectorSource, this.modeControllers[1]?.vectorSource, states);; 
-                        break;
+          switch (this.store.getOptions()?.mode.type) {
+            case 'target':
+              this.modeControllers[0]?.renderCurrentSelection(states);
+              break;
+            case 'select':
+              this.modeControllers[0]?.renderCurrentSelection(states);
+              break;
+            case 'create':
+              this.modeControllers[0]?.renderCurrentSelection(states);
+              break;
+            case 'mix':
+              this.renderUtils.displayMixMode(
+                this.modeControllers[0]?.vectorSource,
+                this.modeControllers[1]?.vectorSource,
+                states
+              );
+              break;
           }
           this.store.getMap()?.updateSize();
-        } else {
-          this.modeControllers.forEach((controller) => controller.removeCurrentSelection())
-          this.store.removeAllSelectedFeatures();
-          this.store.getMap()?.getControls().forEach(control => {
-            if (control instanceof SelectCreateInformationBoxController) {
-              control.disable();
+          if (states.readonly) {
+            const map = this.store.getMap();
+            const options = this.store.getOptions();
+            if (options && map) {
+              map.setView(
+                new View({
+                  projection: 'EPSG:2056',
+                  center:
+                    states.readonly &&
+                    states.currentSelections &&
+                    states.currentSelections.length == 1
+                      ? states.currentSelections[0]
+                      : options.defaultCenter,
+                  zoom:
+                    states.readonly &&
+                    states.currentSelections &&
+                    states.currentSelections.length == 1
+                      ? options.maxZoom
+                      : options.zoom,
+                  minZoom: options.minZoom,
+                  maxZoom: options.maxZoom,
+                  enableRotation: options.interaction.enableRotation,
+                })
+              );
             }
-          })
+          }
+        } else {
+          this.modeControllers.forEach((controller) =>
+            controller.removeCurrentSelection()
+          );
+          this.store.removeAllSelectedFeatures();
+          this.store
+            .getMap()
+            ?.getControls()
+            .forEach((control) => {
+              if (control instanceof SelectCreateInformationBoxController) {
+                control.disable();
+              }
+            });
         }
       }
   }
 
   render() {
     return html`
-    <div id="map" class="${this.store.getTargetBoxSize()} ${Store.getTheme()}">
-    </div>
-    `
+      <div
+        id="map"
+        class="${this.store.getTargetBoxSize()} ${Store.getTheme()}"
+      ></div>
+    `;
   }
 
-  static styles = [unsafeCSS(styles), unsafeCSS(mapStyle), unsafeCSS(controlsStyle), unsafeCSS(notificationStyle), unsafeCSS(theme), unsafeCSS(animationStyle), unsafeCSS(containerStyle)];
+  static styles = [
+    unsafeCSS(styles),
+    unsafeCSS(mapStyle),
+    unsafeCSS(controlsStyle),
+    unsafeCSS(notificationStyle),
+    unsafeCSS(theme),
+    unsafeCSS(animationStyle),
+    unsafeCSS(containerStyle),
+  ];
 }
 
 declare global {
